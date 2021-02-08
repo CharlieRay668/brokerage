4aa6e88f1cc80db7209750e48014fc896167d9fe
CharlieRay668 <charlie.ray320@gmail.com>
2021-02-07 20:41:52 -0500

    removed staticfilesdirs

--- papertrade/settings.py
+++ papertrade/settings.py
@@ -139,11 +139,11 @@ LOGOUT_REDIRECT_URL = "/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 CRISPY_TEMPLATE_PACK = "bootstrap4"
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), ) 
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
