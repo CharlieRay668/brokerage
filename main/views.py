dee2f96e9bbc86c387c0c56062bc9ec47b9cf000
CharlieRay668 <charlie.ray320@gmail.com>
2021-02-13 16:48:31 -0500

    debugging

--- main/views.py
+++ main/views.py
@@ -327,9 +327,9 @@ def get_specific_date(description):
     lookup = {'Jan' : '01','Feb' : '02','Mar' : '03','Apr' : '04','May' : '05','Jun' : '06','Jul' : '07','Aug' : '08','Sep' : '09','Oct' : '10','Nov' : '11','Dec' : '12'}
     description = description.split(' ')
     month = lookup[description[1]]
-    return month
     day = description[2]    
     year = description[3]
+    return (month, day, year)
     if len(day) < 2:
         day = '0'+day
     expiration = dt.date(year, month, day)
