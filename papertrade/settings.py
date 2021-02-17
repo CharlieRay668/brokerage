8a763c1e0e13b6f4c7b1fa254dc9554e7212d603
CharlieRay668 <charlie.ray320@gmail.com>
2021-02-16 22:54:21 -0500

    Mobile Formatting done! hopefully.....

--- papertrade/settings.py
+++ papertrade/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'papertrade.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-if True:
+if False:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
