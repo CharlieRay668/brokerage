6111d9c8158a4c925c9d81fbfcc5dcd75656ecb2
CharlieRay668 <charlie.ray320@gmail.com>
2021-02-18 16:48:17 -0500

    removed rest handler from wsgi

--- papertrade/wsgi.py
+++ papertrade/wsgi.py
@@ -10,20 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from utils.resthandler import RestHandler
-from utils.TDRestAPI import Rest_Account
-import threading
-import time
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'papertrade.settings')
 
 application = get_wsgi_application()
-
-
-REST_API = Rest_Account('keys.json')
-rest_handler = REST_HANDLER = RestHandler(REST_API)
-
-_thread = threading.Thread(target=rest_handler.loop())
-_thread.setDaemon(True)
-_thread.start()
