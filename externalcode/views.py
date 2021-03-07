7ebe0e042dc4446948e76bfde8fbb013cc88ac5c
CharlieRay668 <charlie.ray320@gmail.com>
2021-03-07 11:51:33 -0700

    adding dashboard

---
+++ externalcode/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+
+# Create your views here.
+
+def dashboard(response):
+    return render(response, 'externalcode/dashboard.html')
