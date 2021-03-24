8899a0dd008ba062d9dfe51dd78df93b2624150d
CharlieRay668 <charlie.ray320@gmail.com>
2021-03-23 19:58:55 -0600

    building assignment

--- assignment/assigner.py
+++ assignment/assigner.py
@@ -1,8 +1,12 @@
 from users.models import User
+from main.models import Position
 import datetime as dt
+from utils.TDRestAPI import Rest_Account
 
+REST_API = Rest_Account('keys.json')
 def assign():
-    position_infos = []
+    total_positions = []
+    underlyings = []
     users = User.objects.all()
     for user in users:
         positions = user.positions.all()
@@ -15,5 +19,9 @@ def assign():
                 expiration = dt.date(expiration_year, expiration_month, expiration_day)
                 now = dt.date.today()
                 if now > expiration:
-                    position_infos.append(position_info)
-    return position_infos
+                    total_positions.append(position)
+                    underlyings.append(position_info['underlying'])
+    quotes = REST_API.get_quotes(underlyings)
+    for position in total_positions:
+        strike_price = position.position_info[0]['strikePrice']
+    return quotes
