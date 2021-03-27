eac59b5a93837fb19115efbbcdb40bf90640d359
CharlieRay668 <charlie.ray320@gmail.com>
2021-03-26 20:10:29 -0600

    something wrong with asset_type

--- api/views.py
+++ api/views.py
@@ -248,7 +248,7 @@ def create_position(response):
             account.cash_amount -= quantity*fill_price
         elif asset_type == "OPTION":
             account.cash_amount -= (quantity*fill_price)*100
-        return HttpResponse("Checkpoint", status=204)
+        return HttpResponse("Checkpoint " + str(asset_type), status=204)
         account.acct_positions.add(new_position)
         account.save(update_fields=['option_amount', 'equity_amount', 'cash_amount'])
         user.positions.add(new_position)
