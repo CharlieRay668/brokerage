38e8a96356eb2f14d0e351401f19cea33b497ff7
CharlieRay668 <charlie.ray320@gmail.com>
2021-03-26 20:05:40 -0600

    moved checkpoint

--- api/views.py
+++ api/views.py
@@ -237,9 +237,9 @@ def create_position(response):
         if override_price is not False:
             if api_key == "charliekey":
                 fill_price = override_price
-        return HttpResponse("Checkpoint", status=204)
         new_position = Position(position_id=position_id, symbol=symbol, quantity=quantity, fill_price=fill_price, position_info=position_info, order_action=action, order_type=order_type, order_expiration=order_expiration, order_execution_date=order_execution_date, limit_price=limit_price)
         new_position.save()
+        return HttpResponse("Checkpoint", status=204)
         try:
             account = user.accounts.get(id=account_id)
         except:
